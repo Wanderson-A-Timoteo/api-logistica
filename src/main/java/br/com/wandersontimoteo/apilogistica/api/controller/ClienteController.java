6fc373b69f1cca6c9bc09da25307242b9ec12e1f
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-03-22 19:46:49 -0400

    Corrigido bug em flyway e dependencias, adicionado migration criar tb cliente e adicionado classe ClienteRepository

--- src/main/java/br/com/wandersontimoteo/apilogistica/api/controller/ClienteController.java
+++ src/main/java/br/com/wandersontimoteo/apilogistica/api/controller/ClienteController.java
@@ -1,29 +1,30 @@
 package br.com.wandersontimoteo.apilogistica.api.controller;
 
 import br.com.wandersontimoteo.apilogistica.domain.model.Cliente;
+import br.com.wandersontimoteo.apilogistica.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
+
+    private ClienteRepository clienteRepository;
+
     @GetMapping("/clientes")
     public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+    @GetMapping("/clientes/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 
-        Cliente cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("Wanderson");
-        cliente1.setEmail("wanderson@gmail.com");
-        cliente1.setTelefone("(99) 9.9999-9999");
-
-        Cliente cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Ryan");
-        cliente2.setEmail("ryan@gmail.com");
-        cliente2.setTelefone("(88) 8.8888-8888");
-
-        return Arrays.asList(cliente1, cliente2);
+        return clienteRepository.findById(clienteId)
+                .map(cliente -> ResponseEntity.ok(cliente))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/br/com/wandersontimoteo/apilogistica/domain/model/Cliente.java
+++ src/main/java/br/com/wandersontimoteo/apilogistica/domain/model/Cliente.java
@@ -1,12 +1,20 @@
 package br.com.wandersontimoteo.apilogistica.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
