92551a5c42b4f0c831a80821c2442e212e24d9fd
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-03-24 18:24:30 -0400

    Adicionado metodo PUT de atualização de clientes cadastrados

--- src/main/java/br/com/wandersontimoteo/apilogistica/api/controller/ClienteController.java
+++ src/main/java/br/com/wandersontimoteo/apilogistica/api/controller/ClienteController.java
@@ -3,28 +3,46 @@ package br.com.wandersontimoteo.apilogistica.api.controller;
 import br.com.wandersontimoteo.apilogistica.domain.model.Cliente;
 import br.com.wandersontimoteo.apilogistica.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     private ClienteRepository clienteRepository;
 
-    @GetMapping("/clientes")
+    @GetMapping
     public List<Cliente> listar() {
         return clienteRepository.findAll();
     }
-    @GetMapping("/clientes/{clienteId}")
+    @GetMapping("/{clienteId}")
     public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 
         return clienteRepository.findById(clienteId)
-                .map(cliente -> ResponseEntity.ok(cliente))
+               // .map(cliente -> ResponseEntity.ok(cliente))
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+                                             @RequestBody Cliente cliente) {
+        if(!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(clienteId);
+        cliente = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
     }
 }
