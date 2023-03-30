38742016e2d80874328d4fe2fe5d66dd487266a4
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-03-30 19:52:59 -0400

    Adicionado os Representation Model EntregaModel e DestinatarioModel para separar do domain model e substituido a busca de entregas por id em EntregaCotroller para usar o EntregaModel

--- src/main/java/br/com/wandersontimoteo/apilogistica/api/controller/EntregaController.java
+++ src/main/java/br/com/wandersontimoteo/apilogistica/api/controller/EntregaController.java
@@ -1,5 +1,7 @@
 package br.com.wandersontimoteo.apilogistica.api.controller;
 
+import br.com.wandersontimoteo.apilogistica.api.model.DestinatarioModel;
+import br.com.wandersontimoteo.apilogistica.api.model.EntregaModel;
 import br.com.wandersontimoteo.apilogistica.domain.model.Entrega;
 import br.com.wandersontimoteo.apilogistica.domain.repository.EntregaRepository;
 import br.com.wandersontimoteo.apilogistica.domain.service.SolicitacaoEntregaService;
@@ -31,9 +33,25 @@ public class EntregaController {
     }
 
     @GetMapping("/{entregaId}")
-    public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
+    public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId) {
         return entregaRepository.findById(entregaId)
-                .map(ResponseEntity::ok)
+                .map(entrega -> {
+                    EntregaModel entregaModel = new EntregaModel();
+                    entregaModel.setId(entrega.getId());
+                    entregaModel.setNomeCliente(entrega.getCliente().getNome());
+                    entregaModel.setDestinatario(new DestinatarioModel());
+                    entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
+                    entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
+                    entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
+                    entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
+                    entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
+                    entregaModel.setStatus(entrega.getStatus());
+                    entregaModel.setTaxa(entrega.getTaxa());
+                    entregaModel.setDataPedido(entrega.getDataPedido());
+                    entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
+
+                    return ResponseEntity.ok(entregaModel);
+                })
                 .orElse(ResponseEntity.notFound().build());
     }
 }
---
+++ src/main/java/br/com/wandersontimoteo/apilogistica/api/model/EntregaModel.java
@@ -0,0 +1,21 @@
+package br.com.wandersontimoteo.apilogistica.api.model;
+
+import br.com.wandersontimoteo.apilogistica.domain.model.StatusEntrega;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class EntregaModel {
+
+    private Long id;
+    private String nomeCliente;
+    private DestinatarioModel destinatario;
+    private BigDecimal taxa;
+    private StatusEntrega status;
+    private OffsetDateTime dataPedido;
+    private OffsetDateTime dataFinalizacao;
+}
