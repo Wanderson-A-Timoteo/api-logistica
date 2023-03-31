291e6101aad5eb1b98a876daa6ae101b3a6039c9
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-03-31 14:36:20 -0400

    Adicionado Representation Model ClienteResumoModel passando id e nome do clientre para EntregaModel

---
+++ src/main/java/br/com/wandersontimoteo/apilogistica/api/model/ClienteResumoModel.java
@@ -0,0 +1,12 @@
+package br.com.wandersontimoteo.apilogistica.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClienteResumoModel {
+
+    private Long id;
+    private String nome;
+}
